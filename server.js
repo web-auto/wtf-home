e38b49b96c226a2029ef59188815a2ed227be8d2
Venkat Sundramurthy <venkatesan.sundramurthy@gmail.com>
2014-07-23 00:34:18 -0700

    add wtf home page files

---
+++ server.js
@@ -0,0 +1,11 @@
+var static = require('node-static');
+
+var file = new static.Server('./');
+
+require('http').createServer(function (request, response) {
+  request.addListener('end', function () {
+    file.serve(request, response);
+  }).resume();
+}).listen(8080);
+
+console.log("running server..");
